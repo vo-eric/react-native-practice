fa300b4fd7f6485736596c3c748505332c020670
Eric Vo <vo.eric@gmail.com>
2025-06-30 17:01:40 -0400

    Style primary image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
 import { StyleSheet, View } from 'react-native';
 
@@ -8,6 +9,10 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme='primary' label='Choose a photo' />
+        <Button label='Use this photo' />
       </View>
     </View>
   );
@@ -29,5 +34,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#fff',
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
